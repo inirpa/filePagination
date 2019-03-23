66489a99647225ca855410e991d6230458bedaab
nirparai <nirparai@yahoo.com>
2019-03-23 16:07:58 +0545

    content in popup

--- scan.php
+++ scan.php
@@ -30,7 +30,9 @@ if($total_files>5){
 <div class="row mt-1">
         <?php for($pi=$offset; $pi<$limit; $pi++){ ?>
             <div class="col-2">
-                <i class="fa fa-file" style="color: #518FF5; font-size: 50px;"></i>
+                <a href="<?=$dir_name?>/<?=$files[$pi]?>" class="js-popup_modal" data-filename="<?=$files[$pi]?>" data-directoryname="<?=$dir_name?>">
+                    <i class="fa fa-file" style="color: #518FF5; font-size: 50px;"></i>
+                </a>
                 <p><?=$files[$pi]?></p>
                 <button class="btn btn-sm btn-danger deleteFile" data-filename="<?=$files[$pi]?>" data-directory='<?=$dir_name?>' style="float: right; top: 0px; right: 10px; position: absolute;"><i class="fa fa-trash"></i></button>
             </div>    
@@ -45,3 +47,47 @@ if($total_files>5){
         </div>
     <?php } ?>
 </div>
+<div class="modal" id="infoModal">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <p class="modal-title" id="js-modal_fname"></p>
+        <?php /*
+        <button class="btn btn-sm btn-danger deleteFile" style="float: right; top: 20px; right: 50px; position: absolute; border-radius: 60%" title="delete"><i class="fa fa-trash"></i></button>
+        */ ?>
+        <button type="button" class="close" data-dismiss="modal" title="close">&times;</button>
+      </div>
+      <div class="modal-body" id="js-modal_file_content">
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+    $(document).on('click', '.js-popup_modal', function(e){
+        e.preventDefault();
+        var fileName = $(this).data('filename');
+        var dirName = $(this).data('directoryname');
+        var url = $(this).attr('href');
+        var myModal = $('#infoModal').modal('show');
+        loadModalContent(url, fileName, dirName, $(this), myModal);
+    });
+
+    function loadModalContent(url, fileName, dirName, element, myModal) {
+    $.ajax({
+        type: 'get',
+        url: url,
+        dataType: 'html',
+        beforeSend: function() {
+            myModal.find("#js-modal_file_content").html('<div class="text-center"> <i class="fa fa-spin fa-spinner"></i> </div>');
+        },
+        success: function(data) {
+            $('#js-modal_fname').text(fileName);
+            $('#js-modal_file_content').html('<embed src="'+dirName+'/'+fileName+'" frameborder="0" width="100%" height="auto">');
+        },
+        error: function() {
+            myModal.find("#js-modal_file_content").html('<div class="error"> Error </div>');
+        }
+    })
+    return false;
+}
+</script>
